ee05b2df9c57ef25cb5e4563f0c490f12e8c7f55
Melvin Ernesto Santana Garcia <melvinernestosantanagarcia15@gmail.com>
2022-10-26 15:30:23 -0500

    Add files

---
+++ Const.py
@@ -0,0 +1,19 @@
+# Board dimensions
+DIMENSION=8
+
+# Castling value White
+CASTLING_WHITE=1
+
+# Castling value Black
+CASTLING_BLACK=8
+
+# Size screen
+SIZE=97
+
+# Centling screen
+LEFT=450
+UP=10
+
+# Colors square
+LIGHT='#eeeed2'
+DARK='#769656'
